87ddf460956bfbb1642e430221428c101a040234
Brunopbb <bruno.almeida@ee.ufcg.edu.br>
2026-05-03 15:19:13 -0300

    update

--- src/MEDIDOR_UFCG_ESP8266/secrets.h
+++
@@ -1,9 +0,0 @@
-#define SECRET_SSID "Maria"
-#define SECRET_PASSWORD "12689070499"
-#define SECRET_BROKER "150.165.37.247"
-#define SECRET_MQTT_USER "smartmeter"
-#define SECRET_MQTT_PASS "Lab3*3=1%" // A senha que criamos no Mosquitto
-
-// ATENÇÃO NA PORTA 8080 AQUI!
-#define SECRET_URL_VERSAO "http://150.165.37.247:8080/versao.txt"
-#define SECRET_URL_FIRMWARE "http://150.165.37.247:8080/firmware.bin"
